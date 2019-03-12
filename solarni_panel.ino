8e85862140e8156808decc95b52c97ab0d5bb019
Andrej Savikin <andrejsavikin@gmail.com>
2019-03-13 00:00:13 +0100

    Dodato racunanje X i Y pomeranja

---
+++ solarni_panel.ino
@@ -0,0 +1,61 @@
+#include <Stepper.h>
+const int stepsPerRevolution = 200;
+
+const int maxX = stepsPerRevolution;
+const int maxY = stepsPerRevolution/2;
+const int stepAmount = 10;
+const int errorAmount = 10;
+//TODO polish out values
+
+boolean runningStepper = false;
+int incomingByte = 0;
+Stepper xStepper(stepsPerRevolution, 2,3, 4, 5);
+Stepper yStepper(stepsPerRevolution, 6, 7, 8, 9);
+const int LR1 = A0;//TL
+const int LR2 = A1;//TR
+const int LR3 = A2;//BL
+const int LR4 = A3;//BR
+
+void setup() {
+  // put your setup code here, to run once:
+  xStepper.setSpeed(60);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()){
+    incomingByte = Serial.read();
+    //TODO
+  }
+  
+  int LR1V = analogRead(LR1);
+  int LR2V = analogRead(LR2);
+  int LR3V = analogRead(LR3);
+  int LR4V = analogRead(LR4);
+  int Ymove = getYMove(LR1V, LR2V, LR3V, LR4V);
+  int Xmove = getXMove(LR1V, LR2V, LR3V, LR4V);
+  
+}
+
+int getYMove(int LR1V, int LR2V, int LR3V, int LR4V){
+  int topAvg = (LR1V + LR2V) / 2;
+  int botAvg = (LR3V + LR4V) / 2;
+  int diffAvg = topAvg - botAvg;
+  if(abs(diffAvg)>errorAmount){
+    return diffAvg/abs(diffAvg);
+  }else{
+    return 0;
+  }
+}
+
+int getXMove(int LR1V, int LR2V, int LR3V, int LR4V){
+  int leftAvg = (LR1V + LR3V) / 2;
+  int rightAvg = (LR2V + LR4V) / 2;
+  int diffAvg = leftAvg - rightAvg;
+  if(abs(diffAvg)>errorAmount){
+    return diffAvg/abs(diffAvg);
+  }else{
+    return 0;
+  }
+
